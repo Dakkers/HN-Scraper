824e1187d11bd572708fb36cc30e3cc2b8188bff
Dakota St. Laurent <d.h.stlaurent@gmail.com>
2015-07-09 02:40:49 -0400

    almost entirely working, except for incrementing counts

--- lib/hnscraper.ex
+++ lib/hnscraper.ex
@@ -12,8 +12,17 @@ defmodule Posts do
   use Ecto.Model
 
   schema "posts" do
+    # ID is implicitly created
+    field :url, :string
+  end
+end
+
+defmodule Words do
+  use Ecto.Model
+
+  schema "words" do 
+    field :word, :string
     field :post_id, :integer
-    field :url,     :string
   end
 end
 
@@ -34,10 +43,9 @@ defmodule HNScraper.Repo do
     otp_app: :hnscraper
 end
 
-# actual functions
-defmodule HNScraper do
-  import Ecto.Query
 
+# string helpers
+defmodule HNScraper.Str do
   def replace_punc(s) do
     Regex.replace(~r/[^a-zA-Z_']/, s, " ")
   end
@@ -46,24 +54,98 @@ defmodule HNScraper do
     Regex.replace(~r/ {2,}/, s, " ")
   end
 
-  def word_in_db?(w) do
+  def format_title(title) do
+    title
+    |> replace_punc
+    |> remove_multiple_spaces
+    |> String.downcase
+    |> String.split(" ")
+    |> Enum.filter(&(String.length(&1) > 1))
+  end
+end
+
+
+defmodule HNScraper do
+  HNAPI.start
+  import Ecto.Query
+
+  # DATABASE HELPERS
+  # check if word exists in the Counts table (not the Words table, because the
+  # Enum.count call will be faster this way. #yolo)
+  def word_in_db?(word) do
     query = from row in Counts,
-      where: row.word == ^w,
+      where: row.word == ^word,
       select: row.word
     Enum.count(HNScraper.Repo.all(query)) == 1
   end
 
+  # check if post ID exists in the database
   def post_in_db?(post_id) do
     query = from row in Posts,
-      where: row.post_id == ^post_id,
-      select: row.post_id
+      where: row.id == ^post_id,
+      select: row.id
     Enum.count(HNScraper.Repo.all(query)) == 1
   end
 
+  # check if URL exists in the database
   def url_in_db?(url) do
     query = from row in Posts,
       where: row.url == ^url,
       select: row.url
     Enum.count(HNScraper.Repo.all(query)) == 1
   end
+
+  # insert a word from a title into the "Words" table
+  def insert_word_into_db(word, post_id) do
+    HNScraper.Repo.insert %Words{post_id: post_id, word: word}
+    word
+  end
+
+  # THIS IS NOT CURRENTLY WORKING ~~~~~~~~~~~~~~~~~~~~~~~~~
+  def increment_word_count(word) do
+    # if (word_in_db?(word)) do
+    #   query = from(row in Counts, where: row.word == ^word, select: row.count)
+    #   # current_count = hd(HNScraper.Repo.all(query))
+
+    #   # query = from(row in Counts) |> where([row], row.word == ^word) |> update([row], inc: [count: 1])
+    #   query = from(row in Counts, where: row.word == ^word, update: [set: [count: ^(current_count + 1)]])
+    # end
+    if not word_in_db?(word) do
+      HNScraper.Repo.insert %Counts{word: word, count: 100}
+    end
+    word
+  end
+
+  # insert all stuff into the database!
+  def insert_data_into_db(post) do
+    post_id = post[:id]
+    Enum.map(post[:title], &(increment_word_count(&1)))
+    HNScraper.Repo.insert %Posts{id: post_id, url: post[:url]}
+    Enum.map(post[:title], &(insert_word_into_db(&1, post_id)))
+    post
+  end
+
+  # check if post is ask type, in case post wasn't tagged correctly
+  def is_ask?(post) do
+    Regex.match?(~r/^Ask HN:/, post["title"])
+  end
+
+  def create_post_map(post) do
+    %{
+      :title => HNScraper.Str.format_title(post["title"]),   # a list!
+      :url   => post["url"],
+      :id    => post["id"]
+    }
+  end
+
+  def scrape() do
+    HNAPI.top_stories_by_id("story", 5)
+    |> Enum.filter(&(not post_in_db?(&1)))
+    |> Enum.map(&(HNAPI.get_item(&1)))
+    |> Enum.filter(&(not is_ask?(&1)))
+    |> Enum.filter(&(not url_in_db?(&1["url"])))
+    |> Enum.map(&(create_post_map(&1)))
+    |> Enum.map(&(insert_data_into_db(&1)))
+  end
+  
 end
